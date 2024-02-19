20fb72a2d7c70d161f89f092baea9358e9430065
milanh34 <milanh344@gmail.com>
2024-02-19 14:00:05 +0530

    setup complete

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+.then(() => {
+    app.on("error", (err) => {
+        console.error("Error in connecting MONGODB: ",err)
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Sever is running at PORT ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection Failure: ", err)
+})
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+
+;( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.error("Error: ", error)
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening at PORT ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error(error)
+        throw error
+    }
+})()
+*/
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return "Could not find path"
+        // upload file on cloudinary
+        let res = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // after successful upload
+        console.log("File is uploaded on cloudinary", res.url);
+        return res;
+    } catch (error) {
+        fs.unlinkSync(localFilePath)  //removes locally saved temp files as the upload operation got failed
+        return null;
+    }
+}
+
+export { uploadOnCloudinary }
