257c33d0fc5f2278388b35366839b2908819d237
milanh34 <milanh344@gmail.com>
2024-03-24 14:40:20 +0530

    Added a function to publish videos

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -106,4 +106,80 @@ const getAllVideos = asyncHandler( async( req, res ) => {
     
 })
 
-export { getAllVideos }
+const publishAVideo = asyncHandler( async( req, res ) => {
+    const { title, description } = req.body
+    // TODO: get video, upload to cloudinary, create video
+    // Steps
+    // 1. check title and desc
+    // 2. get local path of files
+    // 3. check if localpath is empty
+    // 4. upload on cloudinary
+    // 5. check if uploaded or not
+    // 6. get user details and check 
+    // 7. create video and check
+    // 8. response
+
+    if(!title || title.trim() === ""){
+        throw new ApiError(400, "Title cannot be empty")
+    }
+    if(!description || description.trim() === ""){
+        throw new ApiError(400, "Description cannot be empty")
+    }
+
+    const videoFileLocalPath = req.files?.videoFile[0]?.path;
+    const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+
+    if(!videoFileLocalPath){
+        throw new ApiError(400, "Video file is required")
+    }
+    if(!thumbnailLocalPath){
+        throw new ApiError(400, "Thumbnail is required")
+    }
+
+    let videoFile = await uploadOnCloudinary(videoFileLocalPath)
+    let thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+
+    if(!videoFile){
+        throw new ApiError(500, "Error while uploading video")
+    }
+    if(!thumbnail){
+        throw new ApiError(500, "Error while upploading thumbnail")
+    }
+
+    const user = req.user?._id || await User.findOne({
+        refreshToken: req.cookies.refreshToken
+    })
+    if(!user){
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    const video = await Video.create({
+        videoFile: videoFile.url,
+        thumbnail: thumbnail.url,
+        title,
+        description,
+        duration: videoFile.duration,
+        createdBy: user
+    })
+    if(!video){
+        throw new ApiError(500, "Something went wrong while creating video")
+    }
+
+    const uploadedVideo = await Video.findById(video._id)
+    if(!uploadedVideo){
+        throw new ApiError(500, "Something went wrong while uploading video")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(
+            200,
+            uploadedVideo,
+            "Video has been uploaded successfully"
+        )
+    )
+
+})
+
+export { getAllVideos, publishAVideo }
