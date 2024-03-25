b931b851184c04b109a14af2fdb9f4374fc1962f
milanh34 <milanh344@gmail.com>
2024-03-25 21:26:39 +0530

    Added a function to add or remove like to a tweet

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -15,8 +15,8 @@ const toggleVideoLike = asyncHandler( async ( req, res ) => {
     // 1. check video id
     // 2. check authorization
     // 3. check if already liked or not
-    //    a. if already liked, delete object
-    //    b. if not liked, create object
+    //    a. if not liked, create object
+    //    b. if already liked, delete object
     // 4. response
 
     const { videoId } = req.params
@@ -40,24 +40,20 @@ const toggleVideoLike = asyncHandler( async ( req, res ) => {
         throw new ApiError(401, "Unauthorized request")
     }
 
-    const hasUserLikedBefore = await Like.findOne({
-        likedBy: user
-    })
+    const likeObject = {
+        likedBy: user,
+        video: video
+    }
+    const hasUserLikedBefore = await Like.findOne(likeObject)
 
     let toggledVideoLike
     let message
     if(!hasUserLikedBefore){
-        toggledVideoLike = await Like.create({
-            video: video,
-            likedBy: user
-        })
+        toggledVideoLike = await Like.create(likeObject)
         message= "Video liked successfully"
     }
     else{
-        toggledVideoLike = await Like.findOneAndDelete({
-            video: video,
-            likedBy: user
-        })
+        toggledVideoLike = await Like.findOneAndDelete(likeObject)
         message= "Like removed from video successfully"
     }
 
@@ -72,4 +68,64 @@ const toggleVideoLike = asyncHandler( async ( req, res ) => {
     )
 })
 
-export { toggleVideoLike }
+const toggleTweetLike = asyncHandler( async ( req, res ) => {
+
+    //TODO: toggle like on tweet
+    // Steps
+    // 1. check tweet id
+    // 2. check authorization
+    // 3. check if already liked or not
+    //    a. if not liked, create object
+    //    b. if already liked, delete object
+    // 4. response
+
+    const { tweetId } = req.params
+
+    if(!tweetId || tweetId.trim() === ""){
+        throw new ApiError(404, "Tweet Id cannot be empty")
+    }
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(404, "Tweet does not exist")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet){
+        throw new ApiError(404, "Tweet doesn't exist")
+    }
+
+    const user = await User.findOne({
+        refreshToken: req.cookies.refreshToken
+    })
+    if(!user){
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    const likeObject = {
+        likedBy: user,
+        tweet: tweet
+    }
+    const hasUserLikedBefore = await Like.findOne(likeObject)
+
+    let toggledTweetLike
+    let message
+    if(!hasUserLikedBefore){
+        toggledTweetLike = await Like.create(likeObject)
+        message= "Tweet liked successfully"
+    }
+    else{
+        toggledTweetLike = await Like.findOneAndDelete(likeObject)
+        message= "Like removed from tweet successfully"
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            toggledTweetLike,
+            message
+        )
+    )
+})
+
+export { toggleVideoLike, toggleTweetLike }
--- src/routes/like.router.js
+++ src/routes/like.router.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleVideoLike } from "../controllers/like.controller.js";
+import { toggleTweetLike, toggleVideoLike } from "../controllers/like.controller.js";
 
 const router = Router();
 
 router.use(verifyJWT)
 router.route("/toggle/v/:videoId").post(toggleVideoLike)
-router.route("/toggle/t/:tweetId").post()
+router.route("/toggle/t/:tweetId").post(toggleTweetLike)
 router.route("/toggle/c/:commentId").post()
 router.route("/videos").get()
 
