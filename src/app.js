87a570cd03e3c9ca7945a8136c71fb65b0baaf54
milanh34 <milanh344@gmail.com>
2024-03-25 23:34:17 +0530

    Renamed a misspelled file

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ import userRouter from './routes/user.routes.js';
 import tweetRouter from './routes/tweet.routes.js';
 import videoRouter from './routes/video.routes.js';
 import commentRouter from './routes/comment.routes.js';
-import likeRouter from './routes/like.router.js';
+import likeRouter from './routes/like.routes.js';
 
 // declare routes
 app.use("/api/v1/users", userRouter)
