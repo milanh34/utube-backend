5097265572f5bdb43a7c3efbc9d660716626bed3
milanh34 <milanh344@gmail.com>
2024-03-26 17:21:40 +0530

    Added a function to reply to a comment

--- src/controllers/reply.contoller.js
+++ src/controllers/reply.contoller.js
@@ -104,4 +104,67 @@ const getCommentReplies = asyncHandler( async ( req, res ) => {
     )
 })
 
-export { getCommentReplies }
+const addReply = asyncHandler( async ( req, res ) => {
+
+    // TODO: add a reply to a comment
+    // Steps 
+    // 1. check comment Id
+    // 2. check if content is empty or not
+    // 3. check authorization
+    // 4. create reply
+    // 5. check if reply is created or not
+    // 6. response
+
+    const { commentId } = req.params
+    const { content } = req.body
+
+    if(!commentId || commentId.trim() === ""){
+        throw new ApiError(404, "Comment Id cannot be empty")
+    }
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(404, "Comment does not exist")
+    }
+
+    if(!content || content.trim() === ""){
+        throw new ApiError(404, "Content cannot be empty");
+    }
+    
+    const user = await User.findOne({
+        refreshToken: req.cookies.refreshToken
+    })
+    if(!user){
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    const comment = await Comment.findById(commentId)
+    if(!comment){
+        throw new ApiError(404, "Comment doesn't exist")
+    }
+
+    const reply = await Reply.create({
+        content: content,
+        comment: comment,
+        repliedBy: user
+    })
+
+    if(!reply){
+        throw new ApiError(500, "Error while replying")
+    }
+    
+    const createdReply = await Reply.findById(reply?._id)
+    if(!createdReply){
+        throw new ApiError(500, "Error while replying")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(
+            200,
+            createdReply,
+            "Replied successfully"
+        )
+    )
+})
+
+export { getCommentReplies, addReply }
